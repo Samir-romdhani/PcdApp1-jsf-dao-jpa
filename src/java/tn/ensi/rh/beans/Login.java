ae14743c8d41d4e6e7792cbdb82cdeb31c9139a9
user <user@DESKTOP-BI1T51L>
2017-04-03 21:25:36 +0100

    login + template vide

---
+++ src/java/tn/ensi/rh/beans/Login.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.ensi.rh.beans;
+
+import tn.ensi.rh.dao.UserDao ;
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+ 
+@ManagedBean(name = "login")
+@SessionScoped
+/**
+ *
+ * @author User
+ */
+public class Login implements Serializable {
+ 
+    private static final long serialVersionUID = 1L;
+    private String password;
+    private String message, uname;
+ 
+    public String getMessage() {
+        return message;
+    }
+ 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+ 
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+ 
+    public String getUname() {
+        return uname;
+    }
+ 
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+ 
+    public String loginProject() {
+        boolean result = UserDao.login(uname, password);
+        if (result) {
+            // get Http Session and store username
+            HttpSession session = Util.getSession();
+            session.setAttribute("username", uname);
+ 
+            return "template.xhtml";
+        } else {
+ 
+            FacesContext.getCurrentInstance().addMessage(
+                    null,
+                    new FacesMessage(FacesMessage.SEVERITY_WARN,
+                    "Invalid Login!",
+                    "Please Try Again!"));
+ 
+            return "login.xhtml";
+        }
+    }
+ 
+    public String logout() {
+      HttpSession session = Util.getSession();
+      session.invalidate();
+      return "login.xhtml";
+   }
+}
